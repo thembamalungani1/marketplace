120f14db62e2f145f1832e0d939c55c27571ab01
Themba Malungani <thembam@simfy.africa>
2021-01-12 02:25:14 +0200

    fix broken form component

--- resources/views/components/form-select-options.blade.php
+++ resources/views/components/form-select-options.blade.php
@@ -2,7 +2,10 @@
     <label for="{{ $name }}" class="block text-gray-500 my-2">{{ $label }}</label>
     <select name="{{ $name }}" class="@error($name) border-red-500 @enderror p-2  form-input text-gray-400 border-gray-300 dark:border-dark-4 input input--lg w-full border block" >
         @foreach($options as $option)
-            <option value="{{ $option['value'] }}" {{ request()->get($name) == $option['value'] ? 'selected' : ''}}>{{ $option['label'] }}</option>
+            <option
+                value="{{ $option['value'] }}"
+                {{ request()->get($name) == $option['value'] || old($name) == $option['value'] ? 'selected' : ''}}
+            >{{ $option['label'] }}</option>
         @endforeach
     </select>
 </div>
