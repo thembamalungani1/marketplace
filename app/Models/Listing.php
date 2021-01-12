4b2d7908084b8be2ec8aad872437ff6ec0d383ea
Themba Malungani <thembam@simfy.africa>
2021-01-12 03:09:25 +0200

    add seller name to contact form

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -24,7 +24,7 @@ class Listing extends Model
 
     public function owner()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function category()
